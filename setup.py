6ef66ab4b40621b1044eb024de232a0be016da0e
Gebin Maxey <gebinmaxey@hotmail.co>
2020-07-22 22:42:14 +0530

    Add new twine instructions

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup
+from setuptools import find_packages
 setup(
   name = 'commanderiot',         # How you named your package folder (MyLib)
-  packages = ['commanderiot'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  packages = find_packages(),   # Chose the same as "name"
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Python HTTP Library for Commander Web API',   # Give a short description about your library
   author = 'Gebin Maxey',                   # Type in your name
